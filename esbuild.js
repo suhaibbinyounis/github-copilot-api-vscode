47004beff9cdc005f633ddd0a8576e57d4cf50d6
Suhaib Bin Younis <suhaibbinyounis@gmail.com>
2026-05-07 22:17:40 +0530

    fix: bundle applicationinsights telemetry dependencies to fix extension activation crash

--- esbuild.js
+++ esbuild.js
@@ -35,7 +35,7 @@ async function main() {
 		sourcesContent: false,
 		platform: 'node',
 		outfile: 'dist/extension.js',
-		external: ['vscode', '@microsoft/applicationinsights-*'],
+		external: ['vscode'],
 		logLevel: 'silent',
 		// Performance optimizations
 		treeShaking: true,
